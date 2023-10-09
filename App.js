2dae16e92d22ffba2ddeb9a78b46139857021b1f
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2023-10-09 14:36:57 +0300

    Tehtävä 15

--- App.js
+++ App.js
@@ -1,41 +1,93 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import * as Speech from 'expo-speech';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { Header, Button, Input, ListItem, Icon} from '@rneui/themed';
+
+const db = SQLite.openDatabase('shoppinglistdb.db');
 
 export default function App() {
-  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
+  const [shoppinglist, setShoppinglist] = useState([]);
 
-  //const getAvailableVoices = async () => {
-  //  const voices = await Speech.getAvailableVoicesAsync();
-  //};
+  // initialize database
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppinglist (id integer primary key not null, amount text, product text);');
+    }, null, updateList); 
+  }, []);
 
-  const speak = () => {
-    //getAvailableVoices();
-    if (text.trim() !== '') {
-      const voiceOptions = {
-        language: 'fi-FI', // Set the desired language and region
-      };
-      Speech.speak(text, voiceOptions);
-    }
-  };
+  // save item
+  const saveItem = () => {
+    db.transaction(tx => {
+        tx.executeSql('insert into shoppinglist (amount, product) values (?, ?);', [amount, product]);    
+      }, null, updateList
+    )
+  }
 
-  const clearText = () => {
-    setText('');
-  };
+  // update list
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppinglist;', [], (_, { rows }) =>
+        setShoppinglist(rows._array)
+      ); 
+    });
+  }
+
+  // delete item
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(`delete from shoppinglist where id = ?;`, [id]);
+      }, null, updateList
+    )    
+  }
+
+  const renderItem = ({ item }) => (
+    <ListItem bottomDivider>
+      <ListItem.Content>
+        <ListItem.Title>{item.product}</ListItem.Title>
+        <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+      </ListItem.Content>
+      <Icon
+        name="delete"
+        size={25}
+        color="red"
+        onPress={() => deleteItem(item.id)}
+      />
+    </ListItem>
+  );
+  
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Text-to-Speech App</Text>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Enter text here"
-        onChangeText={(text) => setText(text)}
-        value={text}
+      <Header
+      centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff' } }}
       />
-      <View style={styles.buttonContainer}>
-        <Button title="Speak" onPress={speak} />
-        <Button title="Clear Text" onPress={clearText} />
+      <Input
+        placeholder='Product' label='PRODUCT'
+        onChangeText={product => setProduct(product)} value={product} 
+      />
+      <Input
+        placeholder='Amount' label='AMOUNT'
+        onChangeText={amount => setAmount(amount)} value={amount} 
+      />
+      <View style={styles.button}>
+        <Button
+          title='Save'
+          type='solid'
+          color='primary'
+          size='md'
+          onPress={saveItem}>
+          <Icon name='save' color='white' />
+          Save
+        </Button>
       </View>
+      <FlatList
+        data={shoppinglist}
+        keyExtractor={item => item.id.toString()} 
+        renderItem={renderItem}
+      />
     </View>
   );
 }
@@ -44,26 +96,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
+    paddingTop: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  textInput: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingLeft: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  button: {
     width: '50%',
+    alignSelf: 'center',
   },
 });
